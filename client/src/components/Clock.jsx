12dee024b29874b50443bfa5fdbbd409570a6926
Ricky Marasigan <rickymarasigan@Rickys-MacBook-Pro.local>
2021-01-27 20:23:12 -0800

    built clock

--- client/src/components/Clock.jsx
+++ client/src/components/Clock.jsx
@@ -1,13 +1,57 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Main = styled.div`
+    display: flex;
+    justify-content: center;
+    font-size: 5em;
+    font-family: Arial;
+`;
+
 class Clock extends React.Component {
     constructor() {
         super();
+
+        this.state = {
+            time: ''
+        }
+
+        this.fetchTime = this.fetchTime.bind(this);
+    }
+
+    componentWillMount() {
+        this.fetchTime();
+    }
+
+    fetchTime() {
+        var date = new Date();
+        var daytime = 'AM';
+        var hour = date.getHours();
+        if (hour >= 12) {
+            daytime = 'PM'
+            if (hour > 12) {
+                hour -= 12;
+            }
+        }
+        var minute = date.getMinutes();
+        if (minute < 10) {
+            minute = `0${minute}`;
+        }
+        var second = date.getSeconds();
+        if (second < 10) {
+            second = `0${second}`;
+        }
+        var time = `${hour}:${minute}:${second} ${daytime}`;
+        this.setState({
+            time
+        });
+        setTimeout(this.fetchTime, 1000);
     }
 
     render() {
-        return <div>Clock</div>
+        return (
+            <Main>{this.state.time}</Main>
+        )
     }
 }
 
